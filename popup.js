16bb698cbc804b8ae0ace942133f2fedd2398409
ferminrp <frodriguezpenelas@gmail.com>
2019-08-14 18:19:18 -0300

    no message

--- popup.js
+++ popup.js
@@ -1,15 +1,28 @@
 function abbrNum(r,t){t=Math.pow(10,t);for(var a=["k","m","b","t"],n=a.length-1;n>=0;n--){var e=Math.pow(10,3*(n+1));if(e<=r){1e3==(r=Math.round(r*t/e)/t)&&n<a.length-1&&(r=1,n++),r+=a[n];break}}return r}
 
-$.getJSON('https://api.unsplash.com/users/ferminrp/statistics?client_id=cf05c7bd686ac28a242a77ef0d1a52cae2cffbc6161801abeab6440d8213d5c2', function(data) {
+$.getJSON('https://simpleanalytics.com/open-startups.xyz.json', function(data) {
     //data is the JSON string
-    console.log(data.downloads.total);
-    var downloads = abbrNum(data.downloads.total,2);
-    var views = abbrNum(data.views.total,2);
-    var likes = abbrNum(data.likes.total,2);
-    $('#downloads').text(downloads);
-    $('#views').text(views);
-    $('#likes').text(likes);
-    chrome.browserAction.setBadgeText({text:downloads});
+    console.log(data.pageviews);
+    var pageviews = abbrNum(data.pageviews,2);
+    $('#open-startups').text(pageviews);
+    /*chrome.browserAction.setBadgeText({text:downloads});*/
+});
+
+$.getJSON('https://simpleanalytics.com/influencersargentina.xyz.json', function(data) {
+    //data is the JSON string
+    console.log(data.pageviews);
+    var pageviews = abbrNum(data.pageviews,2);
+    $('#influencers').text(pageviews);
+    /*chrome.browserAction.setBadgeText({text:downloads});*/
+});
+
+
+$.getJSON('https://simpleanalytics.com/donarg.com.json', function(data) {
+    //data is the JSON string
+    console.log(data.pageviews);
+    var pageviews = abbrNum(data.pageviews,2);
+    $('#donarg').text(pageviews);
+    /*chrome.browserAction.setBadgeText({text:downloads});*/
 });
 
 
