da1a00fc71bef7e9d9d7f7650c35f1e66b22ebd5
ferminrp <frodriguezpenelas@gmail.com>
2019-08-16 07:26:35 -0300

    Revert "no message"
    This reverts commit 16bb698cbc804b8ae0ace942133f2fedd2398409.

--- background.js
+++ background.js
@@ -1,34 +1,14 @@
-const url_openstartups = 'https://simpleanalytics.com/open-startups.xyz.json';
-const url_influencers = 'https://simpleanalytics.com/influencersargentina.xyz.json';
-const url_donarg = 'https://simpleanalytics.com/donarg.com.json';
-
-var total_pageviews = 0
+const url = 'https://api.unsplash.com/users/ferminrp/statistics?client_id=cf05c7bd686ac28a242a77ef0d1a52cae2cffbc6161801abeab6440d8213d5c2';
 
 function abbrNum(r,t){t=Math.pow(10,t);for(var a=["k","m","b","t"],n=a.length-1;n>=0;n--){var e=Math.pow(10,3*(n+1));if(e<=r){1e3==(r=Math.round(r*t/e)/t)&&n<a.length-1&&(r=1,n++),r+=a[n];break}}return r};
 
 chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab) {
-  fetch(url_openstartups)
+  fetch(url)
     .then(response => response.json())
     .then(data => {
-      var new_pageviews = data.pageviews;
-      total_pageviews = 0;
-      total_pageviews = total_pageviews + new_pageviews;
+      console.log(data);
+      var downloads = abbrNum(data.downloads.total,2);
+      chrome.browserAction.setBadgeText({text:downloads});
     })
     .catch(console.error);
-    fetch(url_influencers)
-      .then(response => response.json())
-      .then(data => {
-        var new_pageviews = data.pageviews;
-        total_pageviews = total_pageviews + new_pageviews;
-      })
-      .catch(console.error);
-      fetch(url_donarg)
-        .then(response => response.json())
-        .then(data => {
-          var new_pageviews = data.pageviews;
-          total_pageviews = total_pageviews + new_pageviews;
-        })
-        .catch(console.error);
-        var number = abbrNum(total_pageviews,1)
-        chrome.browserAction.setBadgeText({text:number.toString()});
 });
